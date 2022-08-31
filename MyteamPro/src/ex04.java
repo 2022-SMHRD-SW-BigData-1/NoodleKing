1e22caadb279a85c80fc42ddfa78df286d954f3b
smhrd <smhrd@DESKTOP-TEL5CO1>
2022-08-31 11:35:59 +0900

    치킨

---
+++ MyteamPro/src/ex04.java
@@ -0,0 +1,12 @@
+
+public class ex04 {
+
+	public static void main(String[] args) {
+		System.out.println("파닭");
+		System.out.println("유린기");
+		System.out.println("깐풍기");
+		System.out.println("카라아게");
+
+	}
+
+}
