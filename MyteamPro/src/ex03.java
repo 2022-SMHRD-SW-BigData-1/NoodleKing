db7d4e239889823db0796783913715b784d98ddf
스마트 <@smhrd>
2022-08-31 11:31:09 +0900

    빅타이어 굳

---
+++ MyteamPro/src/ex03.java
@@ -0,0 +1,11 @@
+
+public class ex03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("충돌테스트");
+		System.out.println("클래스달라도 되는지 테스트");
+		System.out.println("빅타이어 바보");
+	}
+
+}
